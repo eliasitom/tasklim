445283437f2a4aa64fef0e3d559e41e0eb1dd70f
Elias Daniel <119717954+eliasitom@users.noreply.github.com>
2023-12-28 20:56:41 -0300

    feat: improve task interaction
    Improve task interaction with a new top bar and make
    slight enhancements to the new task form.

--- src/routes/tasks_route/Task.jsx
+++ src/routes/tasks_route/Task.jsx
@@ -8,7 +8,6 @@ import moment from "moment";
 
 import { FaTrash, FaPalette, FaEdit } from "react-icons/fa";
 
-
 export function Item({ id, activeId }) {
   const [taskData, setTaskData] = useState({});
 
@@ -39,11 +38,13 @@ export function Item({ id, activeId }) {
           activeId && id === activeId ? "var(--color-2-hover)" : "",
       }}
     >
+      <div className="task-header" style={{cursor: "grabbing"}}>
+        </div>
       <p className="task-body">{taskData.body}</p>
       <div className="task-footer">
-      <div className="task-options">
-          <FaTrash onClick={() => console.log("delete")}/>
-          <FaPalette  />
+        <div className="task-options">
+          <FaTrash onClick={() => console.log("delete")} />
+          <FaPalette />
           <FaEdit />
         </div>
         <p>{taskData.createdAt}</p>
@@ -51,23 +52,69 @@ export function Item({ id, activeId }) {
     </div>
   );
 }
+export default function SortableItem({ activeId, id }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
 
-export default function SortableItem(props) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: props.id });
+  const handleRef = React.useRef(null);
+
+  const [taskData, setTaskData] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/get_task/${id}`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        // Convertir la cadena de fecha a un objeto de Moment
+        const dateMoment = moment(res.task.createdAt);
+
+        // Formatear la fecha en el formato deseado
+        const formatedDate = dateMoment.format("MM/DD/YYYY");
+        setTaskData({
+          ...res.task,
+          createdAt: formatedDate,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
-    width: "100%",
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
-    alingItems: "center",
+    alignItems: "center",
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <Item id={props.id} activeId={props.activeId} />
+    <div ref={setNodeRef} style={style}>
+      <div
+        className="task"
+        style={{
+          backgroundColor:
+            activeId && id === activeId ? "var(--color-2-hover)" : "",
+        }}
+      >
+        <div ref={handleRef} className="task-header" {...attributes} {...listeners}>
+        </div>
+        <p className="task-body">{taskData.body}</p>
+        <div className="task-footer">
+          <div className="task-options">
+            <FaTrash onClick={() => console.log("delete")} />
+            <FaPalette />
+            <FaEdit />
+          </div>
+          <p>{taskData.createdAt}</p>
+        </div>
+      </div>
     </div>
   );
 }
